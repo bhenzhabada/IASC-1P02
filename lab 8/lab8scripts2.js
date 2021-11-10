7f1b2811319a7252ed4e62d9c8429ebab877b71c
Bryan Henz- Habada <bh19sc@brocku.ca>
2021-11-10 00:02:07 -0500

    lab 8

---
+++ lab 8/lab8scripts2.js
@@ -0,0 +1,6 @@
+//Sets the initial colours of the three elements on page load
+function mix(){
+document.getElementById("one").style.color="blue";
+document.getElementById("two").style.color="green";
+document.getElementById("three").style.color="red";
+}
